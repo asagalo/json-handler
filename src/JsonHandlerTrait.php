a0c09cf21fb0f86bd7429020135a4ed09058146a
Erico Pedroso <erico.pedroso@vindi.com.br>
2015-12-28 17:14:36 -0200

    fix method return type

--- src/JsonHandlerTrait.php
+++ src/JsonHandlerTrait.php
@@ -23,9 +23,7 @@ trait JsonHandlerTrait
      **/
     public function jsonToArray(string $json) : array
     {
-        $array = self::decodeJson($json, true);
-
-        return $array;
+        return (array) self::jsonToObject($json);
     }
 
     /**
@@ -35,26 +33,13 @@ trait JsonHandlerTrait
      *
      * @return stdClass
      **/
-    public function jsonToObject(string $json) : object
+    public function jsonToObject(string $json) : \stdClass
     {
-        $object = self::decodeJson($json);
+        $object = json_decode($json);
 
-        return $object;
-    }
-
-    /**
-     * @param string  $json
-     * @param boolean $toArray
-     *
-     * @return array|object
-     */
-    private function decodeJson(string $json, bool $toArray = false) : array
-    {
-        $data = json_decode($json, $toArray);
-
-        if(empty($data))
+        if(empty($object))
             throw new \InvalidArgumentException(json_last_error_msg(), json_last_error());
 
-        return $data;
+        return $object;
     }
 }
